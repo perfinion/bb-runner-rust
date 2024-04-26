44342ee757d8af98865d09b06b088c9fde8b3e62
Jason Zaman <jason@perfinion.com>
2024-04-25 20:51:33 -0700

    Import runner.proto
    Needed prost-types dep update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use buildbarn_runner::runner_server::{Runner, RunnerServer};
+use buildbarn_runner::{RunRequest, RunResponse};
+
+pub mod buildbarn_runner {
+    tonic::include_proto!("buildbarn.runner");
+}
+
 fn main() {
     println!("Hello, world!");
 }
