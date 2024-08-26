d85754db206151bf456200aab1babc9f538c6a0f
Jason Zaman <jason@perfinion.com>
2024-08-25 20:03:11 -0700

    Add domain to proto Any type_url

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut type_config: prost_build::Config = prost_build::Config::new();
     type_config.enable_type_names();
+    type_config.type_name_domain(&["."], "type.googleapis.com");
     tonic_build::configure().compile_with_config(
         type_config,
         &["proto/resourceusage/resourceusage.proto"],
